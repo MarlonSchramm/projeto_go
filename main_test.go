11be5f9513fd9a97c499f01d8384d18398e21581
Marlon <Marlon.schramm@hotmail.com>
2025-05-08 14:53:03 -0300

    removendo comentários teste go

--- main_test.go
+++ main_test.go
@@ -44,14 +44,9 @@ func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
 	resposta := httptest.NewRecorder()
 	r.ServeHTTP(resposta, req)
 	assert.Equal(t, http.StatusOK, resposta.Code, "Deveriam ser iguais")
-	//mockDaResposta := `{"API diz":"E ai gui, Tudo beleza?"}`
 	mockDaResposta := `{"API diz":"oi gui, Tudo beleza?"}`
-	//respostaBody, _ := ioutil.ReadAll(resposta.Body)
 
 	respostaBody, _ := io.ReadAll(resposta.Body)
-
-	//var respostaBody2 bytes.Buffer
-	//respostaBody2, _ := resposta.Body
 
 	assert.Equal(t, mockDaResposta, string(respostaBody))
 }
